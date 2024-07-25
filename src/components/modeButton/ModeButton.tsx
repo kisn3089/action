4a46a99528f540dc025cb4be7e5ffd936b3d9d5b
kisn3089 <kisn3089@naver.com>
2024-07-25 13:37:08 +0900

    [Feat]ModeButton.tsx content & onClick handler complete

--- src/components/modeButton/ModeButton.tsx
+++ src/components/modeButton/ModeButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ModeButton = () => {
-  return <div>ModeButton</div>;
+  return <button onClick={() => console.log("Test")}>Button</button>;
 };
 
 export default ModeButton;
