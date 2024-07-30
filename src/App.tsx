09c2974444ec16eb41e56b7fe4620c93298e0ada
kisn3089 <kisn3089@naver.com>
2024-07-30 16:20:18 +0900

    [commt title]

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,9 @@ function App() {
     <>
       <div>
         <img src={viteLogo} className="logo" alt="Vite logo" />
-        <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
