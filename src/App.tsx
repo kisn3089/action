8e745633d65ef4632b325db4b91e1c097791cf7c
kisn3089 <kisn3089@naver.com>
2024-08-01 13:23:26 +0900

    [Test]

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <img src={viteLogo} className="logo" alt="Vite logo" />
         <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Git</h1>
+      <h1>Test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
